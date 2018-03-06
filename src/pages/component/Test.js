3cfde3fa72b6b78061e85295c1afb651de52b2a4
heping <1075446256@qq.com>
2018-03-06 20:02:16 +0800

    备注

--- src/pages/component/Test.js
+++ src/pages/component/Test.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRoute as Router, Route, Link } from "react-router-dom";
 import createG2 from "g2-react";
 import { Stat } from "g2";
+import './style.css'
+
 
 export default class AntvG2 extends Component {
   constructor (props) {
@@ -11,12 +13,14 @@ export default class AntvG2 extends Component {
 
   componentWillMount () {
 
+
   }
 
   render () {
     return <div>
+      <div></div>
+      <hr/>
       <select
-
         onChange={(e) => {
           console.log(e.target.value)
         }}
@@ -26,15 +30,28 @@ export default class AntvG2 extends Component {
         <option value="3">哦哦</option>
         <option value="4">嘻嘻</option>
       </select>
-<hr/>
+      <hr/>
       <input
         type="number"
         max="99"
         name="price"
       />
-      <embed src = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+      <embed src = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"/>
+      <hr/>
+      <div className="test-box">
+        <p>我喜欢橘子</p>
+        <p>我喜欢苹果</p>
+      </div>
+      <hr/>
+      <div className="box-shadow"> box-shadow: 30px 10px 30px 5px blue;4个值得意思：1向右阴影的长度 2 向下 3，模糊度 4扩散度
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <div className=" outline">轮廓是不占地的</div>
+      <div className=" ">轮廓</div>
 
-      />
+
     </div>
   }
 }
